8e8264a7bfb729c8d683931ea8614f6df80a1d8b
lukevanlukevan <lukevrensburg@gmail.com>
2024-10-17 08:56:41 +0200

    Refactor albumart.py for improved readability and maintainability

--- albumart.py
+++ albumart.py
@@ -28,11 +28,10 @@ def gen_art(words, blur_radius, background_color):
 
     draw = ImageDraw.Draw(image)
 
-    text_size = 70  # Fixed text size
-    font_path = "arial.ttf"  # Path to a .ttf font file
+    text_size = 70
+    font_path = "arial.ttf"
     font = ImageFont.truetype(font_path, text_size)
 
-    # Determine text color based on background luminance
     bg_luminance = colorsys.rgb_to_hsv(*background_color)[2]
     color = "black" if bg_luminance > 60 else "white"
 
@@ -46,7 +45,6 @@ def gen_art(words, blur_radius, background_color):
         draw.text((0, y_offset), words, fill=color, font=font)
         y_offset += text_height
 
-    # Blur and save
     blurred_image = image.filter(ImageFilter.GaussianBlur(blur_radius))
 
     img_bytes = io.BytesIO()
@@ -56,33 +54,25 @@ def gen_art(words, blur_radius, background_color):
     return img_bytes
 
 # SPOTIPY
-# Spotify API credentials
 CLIENT_ID = os.getenv('S_CLIENT_ID')
 CLIENT_SECRET = os.getenv('S_CLIENT_SECRET')
 if not platform.processor() == "":
     REDIRECT_URI = 'http://localhost:8501/'
 else:
     REDIRECT_URI = 'https://lv-cover-generator.streamlit.app/'
-# REDIRECT_URI = 'https://smart-playlist.streamlit.app/'
-# REDIRECT_URI = 'https://smart:8501/'
 
-# Define Spotify scope (the permissions you're requesting from the user)
 SCOPE = "ugc-image-upload playlist-modify-public playlist-modify-private playlist-read-private user-library-read"
 
-# Initialize Spotify OAuth object
 sp_oauth = SpotifyOAuth(client_id=CLIENT_ID,
                         client_secret=CLIENT_SECRET,
                         redirect_uri=REDIRECT_URI,
                         scope=SCOPE)
 
-# Display the Spotify login button
 auth_url = sp_oauth.get_authorize_url()
 
-# Capture the token after redirection
 code = st.query_params.get("code")
 if 'code' not in st.session_state:
     st.session_state['code'] = code
-    # st.query_params.clear()
 
 usecode = st.session_state['code']
 
@@ -91,22 +81,27 @@ if usecode:
     token_info = sp_oauth.get_access_token(usecode)
     sp = spotipy.Spotify(auth=token_info['access_token'])
 
-    # Streamlit UI
     st.title("Playlist Cover Generator")
 
     link = st.text_input('Playlist link')
     blur_radius = 4
-    if 'background_color' not in st.session_state:
-        background_color = hsv_to_rgb(random.randint(0, 360), random.randint(40, 80), random.randint(40, 80))
-    else:
-        background_color = st.session_state.background_color
-        
+
+    if 'background_color' not in st.session_state or st.button("Re-randomize Image"):
+        st.session_state['background_color'] = hsv_to_rgb(random.randint(0, 360), random.randint(40, 80), random.randint(40, 80))
+        if 'img_bytes' in st.session_state:
+            del st.session_state['img_bytes']
+
+    background_color = st.session_state['background_color']
+
     if link:
         playlist_id = link
         playlist = sp.playlist(playlist_id)
         tracks_names = " ".join([re.sub(r'[^a-zA-Z\s]', '', track['track']['name']).strip().lower() for track in playlist['tracks']['items']])
         if tracks_names:
-            img_bytes = gen_art(tracks_names, blur_radius, background_color)
+            if 'img_bytes' not in st.session_state:
+                st.session_state['img_bytes'] = gen_art(tracks_names, blur_radius, background_color)
+
+            img_bytes = st.session_state['img_bytes']
             st.image(img_bytes, use_column_width=True)
 
             if st.button("Change playlist cover"):
@@ -116,9 +111,7 @@ if usecode:
                 jpeg_bytes = jpeg_buffer.getvalue()
                 img_base64 = base64.b64encode(jpeg_bytes).decode('utf-8')
 
-                # Upload base64-encoded image
                 sp.playlist_upload_cover_image(playlist_id, img_base64)
 
 else:
-    st.markdown("<a target='_self' href='" + auth_url + "'>Login with Spotify</a>", unsafe_allow_html=True)
-    #st.link_button("Authorize with Spotify", auth_url)
+    st.link_button("Authorize with Spotify", auth_url)
